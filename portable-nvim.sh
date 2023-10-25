0be00bc97a710fb270902ab80bc3b29d17c605be
Aguiar575 <aguiarus99arthurus@gmail.com>
2023-10-24 23:09:32 -0300

    support for costom config for windows env

--- portable-nvim.sh
+++ portable-nvim.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-URL="https://github.com/neovim/neovim/releases/latest/download/nvim-win64.msi"
+if [ -n "$1" ]; then
+    REPO_URL="$1"
+    CONFIG_DIR="config/nvim"
 
+    echo "Downloading Neovim configuration files from $REPO_URL..."
+    rm -rf "$CONFIG_DIR/*"
+    git clone "$REPO_URL" "$CONFIG_DIR/"
+
+else
+    echo "No REPO_URL specified. Skipping download-config and clean-config."
+fi
+
+URL="https://github.com/neovim/neovim/releases/latest/download/nvim-win64.msi"
 TARGET_DIR="nvim-win64"
 
 curl -L -o nvim-win64.msi "$URL"
